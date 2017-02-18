3a758c644e724ef9974c4dbabda3af7ba4596adb
jenningsjin <jinjenni@umich.edu>
2017-02-17 20:12:30 -0500

    initial file

---
+++ first_snakes.py
@@ -0,0 +1,73 @@
+# Code is text, but it's text you pop into another program 
+# (called either a compiler # or interpreter) that let's you run it and do something
+# Python is a language that uses an interpreter
+# These are comments, which your interpreter ignores
+# Every Language does comments differently, but in python if a line has '#' infront of it, it's a comment
+
+###### Variables and Data Types ###########################################################################
+
+# A program, does something it can really be anything!
+# To do this, you're going to need variables, which are containers for values
+# You can name them whatever you want!
+variable_name = True
+this_is_a_variable = 2
+number_of_dogs = 3.0
+do_you_see_a_pattern = "maybe"
+
+you_can_over_write_variables_after_declaring_then_as_well = 1 
+you_can_over_write_variables_after_declaring_then_as_well = 2
+# The Value of the variable "you_can_over_write_variables_after_declaring_then_as_well" will now be 2
+
+# okay, so you might be wondering what these variables can be holding?
+# Prepare for a couple definitions: Unfortunately, you just gotta memorize them
+# these are your data types:
+
+
+# Integers
+integer_example1 = 0
+integer_example2 = 1
+# These are whole numbers going from -infinity to infinity
+
+# Floats
+float_example1 = 1.0
+float_example2 = 2.0
+float_example3 = 3.23424
+#These are decimal numbers
+
+# Strings
+string_example1 = "words using the latin alphabet"
+string_example2 = "they can be super duper...duper long"
+string_example3 = "or"
+string_example4 = "short"
+string_example5 = "!"
+an_empty_string = ""
+# You can put most alpha-numeric characters and most of the the rest of the characters 
+# on your keyboard, but there are some exceptions
+
+# Booleans (True and False)
+boolean1 = True
+boolean2 = False
+# Booleans simply carry the value of "True" or "False," this will become relevant in a bit
+# when you can tell a program to do something if a variable is True or False
+
+# Lists
+a_list_of_numbers = [1,2,3,4]
+a_list_of_words = ["hi", "I'm", "Jennings", "How", "are", "you", "?"]
+a_list_of_floats = [1.0, 2.0, 3.0, 4.0]
+a_list_of_bools = [True, True, False, False]
+# In python, Lists (or as they are generally called across programming languages: Arrays) are just that:
+# lists of things
+
+
+##### Operators ###########################################################################################
+
+
+###### Control Statements #################################################################################
+
+###### Loops ##############################################################################################
+
+###### Functions ##########################################################################################
+
+
+
+###### Libaries ###########################################################################################
